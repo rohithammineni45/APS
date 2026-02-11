b5790133bab513d05b358698318d67b4a31954e3
rohithammineni45 <rohithammineni67@gmail.com>
2026-02-11 10:59:38 +0530

    Time: 2 ms (88.16%), Space: 46.5 MB (39.27%) - LeetHub

---
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int j = 0; // pointer for popped array
+
+        for (int x : pushed) {
+            stack.push(x);
+
+            // Pop while top matches popped sequence
+            while (!stack.isEmpty() && stack.peek() == popped[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
